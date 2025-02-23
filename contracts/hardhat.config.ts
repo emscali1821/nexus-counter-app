d63e7ff3467a5ed887c6754ae526a91a19d3127a
cryptokmol <129368794+emscali1821@users.noreply.github.com>
2025-02-23 13:24:08 +0800

    Update hardhat.config.ts

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x24b0e8ab4dccf723fb75c0060befea37866498444d562368ef858a3747e79c34";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
